a7c9b1608acc248e8b8fb2fc73d1b4eadb85510a
Luca Sepe <sepelu@valueteam.com>
2019-03-28 15:21:53 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucasepe/cli
+
+go 1.12
---
+++ help.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func createHelpCommand(tool *Tool, w io.Writer) *Command {
+	action := func() error {
+		// The first parameter passed to the help command should be the
+		// command for requested documentation.
+		if len(os.Args) < 3 {
+			tool.printDefaultHelp(w)
+		} else {
+			cmdName := os.Args[2]
+			cmd := tool.getCommand(cmdName)
+
+			if cmd != nil && cmdName != "help" {
+				fmt.Fprintf(w, "%s %s\n%s", tool.binaryName, cmdName, cmd.Documentation)
+
+				if cmd.FlagInit != nil {
+					cmd.FlagInit(&cmd.flagSet)
+
+					fmt.Fprintf(w, "\noptions\n")
+
+					cmd.flagSet.SetOutput(w)
+					cmd.flagSet.PrintDefaults()
+				}
+			} else {
+				tool.printDefaultHelp(w)
+			}
+		}
+
+		return nil
+	}
+
+	cmd := NewCommand("help", "get more information about a command", action)
+	return cmd
+}
